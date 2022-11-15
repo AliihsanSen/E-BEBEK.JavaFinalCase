9dbc9a3da26a43a258876622b4a75f0efbf68c2b
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-15 16:26:58 +0300

    15.11.2022

--- src/PatikaStore1/Main.java
+++ src/PatikaStore1/Main.java
@@ -2,6 +2,7 @@ package PatikaStore1;
 
 public class Main {
     public static void main(String[] args) {
-        new PatikaStore();
+        PatikaStore patikaStore = new PatikaStore();
+        patikaStore.run();
     }
 }
