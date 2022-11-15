6e6c419ee8df887a1e165e610b31de12ee7c6c24
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-15 16:12:45 +0300

    15.11.2022

--- src/PatikaStore1/PhoneComparatorID.java
+++ src/PatikaStore1/PhoneComparatorID.java
@@ -1,4 +1,14 @@
 package PatikaStore1;
 
-public class PhoneComparatorID {
+import java.util.Comparator;
+
+public class PhoneComparatorID implements Comparator<Phone> {
+
+    @Override
+    public int compare(Phone o1, Phone o2) {
+        int id1 = o1.getId();
+        int id2 = o2.getId();
+        return id1 - id2;
+    }
+
 }
