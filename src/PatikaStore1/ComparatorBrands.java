5513c8f4d0538d36b63f8f968d98333cdc6c941b
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-15 16:14:28 +0300

    15.11.2022

--- src/PatikaStore1/ComparatorBrands.java
+++ src/PatikaStore1/ComparatorBrands.java
@@ -1,4 +1,13 @@
 package PatikaStore1;
 
-public class ComparatorBrands {
+import java.util.Comparator;
+
+public class ComparatorBrands implements Comparator<Brands> {
+    @Override
+    public int compare(Brands o1, Brands o2) {
+        String name1 = o1.brand();
+        String name2 = o2.brand();
+        return name1.compareTo(name2);
+    }
+
 }
